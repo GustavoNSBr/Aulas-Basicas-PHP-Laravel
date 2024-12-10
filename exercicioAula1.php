7aa63f570c12a2ace0fe6deeb482f77b3b438f55
GustavoNSBr <gustavonunessilvabr@gmail.com>
2024-12-10 15:58:20 -0300

    adicionando porcentagem do desconto e deixando o codigo antigo

--- exercicioAula1.php
+++ exercicioAula1.php
@@ -1,44 +1,90 @@
 <?php
 
 $produtos = [
-    ['nome' => 'Leite', 'desconto' => 4, 'preco' => '4,49'],
-    ['nome' => 'Condicionador', 'desconto' => 8, 'preco' => '18,17'],
-    ['nome' => 'Biscoito', 'desconto' => 0, 'preco' => '5,80'],
-    ['nome' => 'Mussarela', 'desconto' => 11, 'preco' => '8,15'],
+    ['nome' => 'Leite', 'desconto' => 0.2, 'preco' => '4.12'],
+    ['nome' => 'Condicionador', 'desconto' => 0.7, 'preco' => '10.17'],
+    ['nome' => 'Biscoito', 'desconto' => 0.05, 'preco' => '5.80'],
+    ['nome' => 'Mussarela', 'desconto' => 0, 'preco' => '9.15'],
 ];
 
-foreach($produtos as $produto){
+$porcentagemDesconto = $produto['desconto'] * 100;
 
-    $textoBomProduto = "O Produto " . $produto['nome'] . ' esta com um bom desconto pois custando ' . $produto['preco'] . " esta valendo a pena \n\n";
-    $textoProdutoOkay = "O Produto " . $produto['nome'] . ' esta normal pois esta custando ' . $produto['preco'] . "\n\n";
+foreach ($produtos as $produto) {
 
+    if ($porcentagemDesconto >= 30) {
+        echo "O Produto " . $produto['nome'] . " esta com um bom desconto, custando " . $produto['preco'] . "\n\n";
 
-    if($produto['desconto'] >= 8){
-        echo $textoBomProduto;
-    }
+    } elseif ($porcentagemDesconto >= 5 && $porcentagemDesconto < 30) {
+        echo "O Produto " . $produto['nome'] . " esta com um desconto razoavel, custando " . $produto['preco'] . "\n\n";
 
-    if($produto['desconto'] < 8){
-        echo $textoProdutoOkay;
+    } elseif ($porcentagemDesconto < 5 && $porcentagemDesconto > 0){
+        echo "O Produto " . $produto['nome'] . " esta com um desconto baixo, custando " . $produto['preco'] . "\n\n";
+
+    } else {
+        echo "O Produto " . $produto['nome'] . " esta com sem desconto, custando " . $produto['preco'] . "\n\n";
     }
 }
 
-foreach($produtos as $key => $produto){
-    echo $produtos[$key]['nome'];
-    echo "\n";
+foreach ($produtos as $key => $produto) {
+    echo $produtos[$key]['nome'] . "\n";
+    $porcentagemDesconto = ($produtos[$key]['desconto'] * 100);
 
-    if($produtos[$key]['desconto'] < 1){
+    if ($porcentagemDesconto == 0) {
         echo "O Produto esta sem desconto \n";
-    }
 
-    elseif($produtos[$key]['desconto'] > 5 && $produtos[$key]['desconto'] <= 10){
+    } elseif ($porcentagemDesconto > 5 && $porcentagemDesconto <= 30) {
         echo "O Produto esta com um desconto razoavel! \n";
-    }
 
-    elseif($produtos[$key]['desconto'] <= 5){
+    } elseif ($porcentagemDesconto <= 5) {
         echo "O Produto esta com um desconto baixo! \n";
-    }
 
-    else{
+    } else {
         echo "O Produto esta com um bom desconto! \n";
     }
 }
+
+
+/*
+
+
+$produtos = [
+    ['nome' => 'Leite', 'desconto' => 0.2, 'preco' => '4.49'],
+    ['nome' => 'Condicionador', 'desconto' => 8, 'preco' => '18.17'],
+    ['nome' => 'Biscoito', 'desconto' => 0.5, 'preco' => '5.80'],
+    ['nome' => 'Mussarela', 'desconto' => 0, 'preco' => '8.15'],
+];
+
+foreach ($produtos as $produto) {
+    $precoOriginal = $produto['preco'] + $produto['desconto'];
+    $porcentagemDesconto = ($produto['desconto'] / $precoOriginal) * 100;
+
+    if ($porcentagemDesconto > 30) {
+        echo "O Produto " . $produto['nome'] . " esta com um bom desconto, custando " . $produto['preco'] . "\n\n";
+
+    } elseif ($porcentagemDesconto > 5) {
+        echo "O Produto " . $produto['nome'] . " esta com um desconto razoavel, custando " . $produto['preco'] . "\n\n";
+
+    } else {
+        echo "O Produto " . $produto['nome'] . " esta com um desconto baixo, custando " . $produto['preco'] . "\n\n";
+    }
+}
+
+foreach ($produtos as $key => $produto) {
+    echo $produtos[$key]['nome'] . "\n";
+    $precoOriginal = $produtos[$key]['preco'] + $produtos[$key]['desconto'];
+    $porcentagemDesconto = ($produtos[$key]['desconto'] / $precoOriginal) * 100;
+
+    if ($porcentagemDesconto == 0) {
+        echo "O Produto esta sem desconto \n";
+
+    } elseif ($porcentagemDesconto > 5 && $porcentagemDesconto <= 30) {
+        echo "O Produto esta com um desconto razoavel! \n";
+
+    } elseif ($porcentagemDesconto <= 5) {
+        echo "O Produto esta com um desconto baixo! \n";
+
+    } else {
+        echo "O Produto esta com um bom desconto! \n";
+    }
+}
+*/
